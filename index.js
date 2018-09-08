3db62721208b862887f1aef19ca52ae09d4b8869
BWitz <bhorowitz2014@gmail.com>
2018-09-08 13:20:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function nowServing(katzDeliLine) {
   } 
 }
 
-function currentLine(katzDeliLine){
-  if (katzDeliLine > 0) {
-    return "The line is currently: 1." + katzDeliLine[0] + "2." + katzDeliLine[1] + "3." + katzDeliLine[2];
+function currentLine(line){
+  if (line > 0) {
+    return "The line is currently: 1." + line[0] + "2." + line[1] + "3." + line[2];
   } else {
     return "The line is currently empty.";
   }
