208edd9f140f7dc4b14a4fd470cf9fd4ffa910c8
BWitz <bhorowitz2014@gmail.com>
2018-09-08 12:39:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    katzDeliLine.shift[0];
+    let served = katzDeliLine.shift[0];
   }
 }
