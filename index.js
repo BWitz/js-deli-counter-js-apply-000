b2dcff4140e65ee3cb1ddff41ad0eaee3b338cec
BWitz <bhorowitz2014@gmail.com>
2018-09-08 12:38:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function takeANumber(current, Ada) {
   return "Welcome, " + Ada + ". You are number " + current.length + " in line.";
 }
 
+function nowServing(katzDeliLine) {
+  if (katzDeliLine === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    katzDeliLine.shift[0];
+  }
+}
