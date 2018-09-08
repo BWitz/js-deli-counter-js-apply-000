76791cd41b8f626c94529612e348b03497cb9dc6
BWitz <bhorowitz2014@gmail.com>
2018-09-08 12:35:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(current, Ada) {
   current.push(Ada);
-  return "Welcome," + Ada + "You are number " + current.length + "in line.";
+  return "Welcome, " + Ada + "You are number " + current.length + "in line.";
 }
 
