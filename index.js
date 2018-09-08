be4464c5b6621726d9307ad7e3f61f79d01bee35
BWitz <bhorowitz2014@gmail.com>
2018-09-08 13:41:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(line){
-  if (line === 0) {
-    return "The line is currently empty.";
-  } else {
+  if (line > 0) {
     return `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`;
+  } else {
+    return `The line is currently empty`;
   }
 }
