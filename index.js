59770f070a29c010ca3d639bc4314145ee794792
BWitz <bhorowitz2014@gmail.com>
2018-09-08 12:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function takeANumber(current, Ada) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    return "Currently serving " + katzDeliLine[0] + "." &&
-    katzDeliLine.shift[0];
+    return "Currently serving " + katzDeliLine.shift[0] + ".";
   } else {
     return "There is nobody waiting to be served!";
   } 
