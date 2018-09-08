cea817cb3b7adad74e7e392678501cdf3d10c174
BWitz <bhorowitz2014@gmail.com>
2018-09-08 13:39:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function currentLine(line){
   if (line === 0) {
     return "The line is currently empty.";
   } else {
-    return `The line is currently: 1. ${line[0]},  2. ${line[1]},  3. ${line[2]}`;
+    return `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`;
   }
 }
